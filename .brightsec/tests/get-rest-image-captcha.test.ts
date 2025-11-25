b464442731cb593ecec8d9344aae84cdcb23489c
Viachaslau <slava@tssbox.com>
2025-11-25 15:32:54 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/get-rest-image-captcha.test.ts
@@ -0,0 +1,43 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('GET /rest/image-captcha', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'xss', 'id_enumeration', 'improper_asset_management'],
+      attackParamLocations: [AttackParamLocation.HEADER],
+      starMetadata: {
+        code_source: "tssbox/juice-shop:master",
+        databases: ["SQLite"],
+        user_roles: {
+          roles: ["customer", "deluxe", "accounting", "admin"]
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/rest/image-captcha`,
+      headers: { 'X-Recruiting': 'We are hiring! Visit our careers page for more information.' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});
---
+++ .brightsec/tests/post-rest-2fa-verify.test.ts
@@ -0,0 +1,47 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /rest/2fa/verify', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['csrf', 'jwt', 'xss', 'osi', 'sqli', 'nosql'],
+      attackParamLocations: [AttackParamLocation.BODY],
+      starMetadata: {
+        code_source: "tssbox/juice-shop:master",
+        databases: ["SQLite"],
+        user_roles: {
+          roles: ["customer", "deluxe", "accounting", "admin"]
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/rest/2fa/verify`,
+      body: {
+        tmpToken: "sampleTmpToken123",
+        totpToken: "123456"
+      },
+      headers: { 'Content-Type': 'application/json' },
+      auth: process.env.BRIGHT_AUTH_ID
+    });
+});
